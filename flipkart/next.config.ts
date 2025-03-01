6d76ee62f8ffd7d6bbfa1cb4606d2da7539c882a
OMM KALYAN <ommkalyan3@gamil.com>
2025-03-01 22:34:35 +0530

    deploy

--- flipkart/next.config.ts
+++ flipkart/next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: "/Mern",
+  assetPrefix: "/Mern/"
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
